782b502d65577abb3cf7891d94349de2ef1ebd69
OrigamiAztec <origamiaztec@gmail.com>
2019-11-11 18:32:52 -0600

    now sends data to mongodbatlas

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -8,7 +8,18 @@ app.use(express.static('C:/Users/origa/Documents/GitHub/HackUTD/Flight_Input'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+var MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/";
+
+const url = "mongodb+srv://MyMongoDBUser:PASS@cluster0-k5ros.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+var dbConn = mongodb.MongoClient.connect(url);
 
 
 
